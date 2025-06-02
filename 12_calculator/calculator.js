46887f1d293e9e327a578b96e54974b610b11a32
Nasana <150105128+Krisnaarji@users.noreply.github.com>
2025-06-03 01:33:59 +0800

    completed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
-};
+const add = function(number1, number2) {
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+    return number1 + number2
 
 };
 
-const power = function() {
+const subtract = function(number1, number2) {
 	
+    return number1 - number2
+
+
 };
 
-const factorial = function() {
+const sum = function(numbers) {
 	
+  let total = 0; 
+    for (let i = 0; i < numbers.length; i++) { 
+        total += numbers[i];
+    }
+    return total; 
+
+};
+
+const multiply = function(numbers) {
+
+  let total = 1; 
+    for (let i = 0; i < numbers.length; i++) { 
+        total = total * numbers[i];
+    }
+    return total;
+
+};
+
+const power = function(numbers1, numbers2) {
+	
+
+    
+    return Math.pow(numbers1, numbers2)
+
+};
+
+const factorial = function(n) {
+	
+  if (n < 0) return undefined; // factorial not defined for negative numbers
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+      result *= i;
+    }
+  return result;
+
 };
 
 // Do not edit below this line
