bd9f025a1fbd03f61f8d12b28c741cefbffe63ed
Nasana <150105128+Krisnaarji@users.noreply.github.com>
2025-06-03 14:37:51 +0800

    completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (words) {
+
+    const real = words.toLowerCase().replace(/[^a-z0-9]/g, '')
+
+    let backwards = [];
+
+    for (let j = real.length - 1; j >= 0; j--) {
+        backwards.push(real[j]);
+    }
+
+    const reversed = backwards.join('');  
+
+    return real === reversed;
 
 };
 
