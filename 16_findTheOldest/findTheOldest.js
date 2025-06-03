ddef6461a11b1bbbe19d538610940009fe74ce12
Nasana <150105128+Krisnaarji@users.noreply.github.com>
2025-06-03 18:55:00 +0800

    completed

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
+    return people.reduce((oldest, person) => {
+    const personDeathYear = person.yearOfDeath || currentYear;
+    const oldestDeathYear = oldest.yearOfDeath || currentYear;
+
+    const age = personDeathYear - person.yearOfBirth;
+    const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+    if (age > oldestAge) {
+        return person;
+    } else {
+        return oldest;
+    }
+  });
 };
 
 // Do not edit below this line
