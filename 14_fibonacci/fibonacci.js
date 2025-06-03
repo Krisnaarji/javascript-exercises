be832ece0a5103ea0d5b80b7e489e9c62c6a18dc
Nasana <150105128+Krisnaarji@users.noreply.github.com>
2025-06-03 15:10:06 +0800

    completed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1
+
+    }
+    else if (n < 0) {
+        return "OOPS"
+
+    }
+    else {
+
+        let prev = 0, curr = 1;
+        for (let i = 2; i <= n; i++) {
+          [prev, curr] = [curr, prev + curr];
+      
+        }
+        return curr;
+    }
+
+    
 
 };
 
